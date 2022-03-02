7c126620622e58303cf3e8cca4f689da86d49403
Krisztian Bui <buikrisz00@gmail.com>
2022-03-02 15:26:57 +0100

    smhsmh

--- frontend/script.js
+++ frontend/script.js
@@ -16,9 +16,25 @@ const header = (logo) => {
     `
 }
 
-let countryCards = `
+const countryCard = (name, short, population, flag, continent) => {
+    return `
+        <div class="countryCard">
+            <h2>${name}</h2>
+            <h3>${short}</h2>
+            <h4>${population}</h2>
+            <h5>${continent}</h2>
+            <img>${flag}</img>
+        </div>
+    `;
+}
 
-`
+const countryCards = (countries, callCountryCard) => {
+    let toReturn = "";
+    for (const country of countries) {
+        toReturn += callCountryCard(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0]);
+    }
+    return toReturn;
+}
 
 const loadEvent = async _ => {
     //Get Data
@@ -33,6 +49,7 @@ const loadEvent = async _ => {
 
     const rootElement = document.getElementById("root");
     rootElement.insertAdjacentHTML("beforeend", header("Countries"));
+    rootElement.insertAdjacentHTML("beforeend", countryCards(countries, countryCard));
 }
 
 window.addEventListener("load", loadEvent);
