e98e193bc61695e7aaf756d18e6a177cebafea65
Krisztian Bui <buikrisz00@gmail.com>
2022-03-02 15:13:36 +0100

    smh

--- frontend/script.js
+++ frontend/script.js
@@ -1,79 +1,38 @@
-// get Data
-
-function Month(name, id, nth, days) {
+function Country(name, short, population, flag, continent) {
     this.name = name;
-    this.id = id;
-    this.nth = nth;
-    this.days = days;
+    this.short = short;
+    this.population = population;
+    this.flag = flag;
+    this.continent = continent;
 }
 
-const months = [
-    new Month("January", "jan", 1, 31),
-    new Month("February", "feb", 2, 28),
-    new Month("March", "mar", 3, 31),
-    new Month("April", "apr", 4, 30),
-    new Month("May", "may", 5, 31),
-    new Month("June", "jun", 6, 30),
-    new Month("July", "jul", 7, 31),
-    new Month("August", "aug", 8, 31),
-    new Month("September", "sep", 9, 30),
-    new Month("October", "oct", 10, 31),
-    new Month("November", "nov", 11, 30),
-    new Month("December", "dec", 12, 31)
-];
-
-// prepare Data
-
-// components = HTML elements we would like to add to the document later
-
-const monthSection = (id, h1, days) => {
+//Components
+const header = (logo) => {
     return `
-        <section id="${id}">
-            <h1>${h1}</h1>
-            <div class="days">${days}</div>
-        </section>
-    `;
+    <header>
+        <a id="logo">${logo}</a>
+        <button></button>
+    </header>
+    `
 }
 
-const dayCard = (year, month, day) => {
-    return `
-        <div class="card">
-            <time>${year}</time>
-            <time>${month}</time>
-            <time>${day}</time>
-            <button class="card-btn">Get day name</button>
-        </div>
-    `;
-}
+let countryCards = `
 
-const dayCards = (month, callDayCard) => {
-    let toReturn = "";
-    for (let i = 1; i <= month.days; i++) {
-        toReturn += callDayCard(2022, month.nth, i);
-    }
+`
 
-    return toReturn;
-}
+const loadEvent = async _ => {
+    //Get Data
+    const countryRes = await fetch("https://restcountries.com/v3.1/all");
+    const countryArr = await countryRes.json();
+    
+    //Process Data
+    let countries = countryArr.map(function (country) {
+        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0]);
+    })
+    console.log(countries);
 
-// render = add the components to the document
-
-const loadEvent = _ => {
     const rootElement = document.getElementById("root");
-    let htmlContent = "";
-
-    for (const month of months) {
-        htmlContent += monthSection(month.id, month.name, dayCards(month, dayCard));
-    }
-
-    rootElement.insertAdjacentHTML("beforeend", htmlContent);
-
-    // ClickEvent
-    function clickEvent(event) {
-        if (event.target.classList.contains("card-btn")) {
-            event.target.innerHTML = "This button was clicked";
-        }
-    }
-    document.addEventListener("click", clickEvent);
+    rootElement.insertAdjacentHTML("beforeend", header("Countries"));
 }
 
 window.addEventListener("load", loadEvent);
