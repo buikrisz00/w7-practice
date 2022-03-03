878a727f13ea57ace772655e6b6046f28d2f10bd
Krisztian Bui <buikrisz00@gmail.com>
2022-03-03 17:16:51 +0100

    fasf

--- frontend/script.js
+++ frontend/script.js
@@ -1,32 +1,44 @@
-function init(mathFunction) {
-    var a = 5;
-    var b = 6;
-   
-    if (a < b) {
-        let c = mathFunction(b, a)
-        /* (function(){
-            var c = b - a;
-        })(); */
-        console.log(c);
-    } else {
-        let c = mathFunction(a, b)
-       /*  (function(){
-            var c = a - b;
-        })(); */
-        console.log(c);
-    }
+// Load esemény + form + 3 input mező (szöveges) + gomb
+// Minden input mezőre input esemény menjen rá --> console.logolja ki az értékét az input mezőnek
+
+function loadEvent() {
+    const rootElement = document.getElementById("root");
+    rootElement.insertAdjacentHTML("beforeend", `
+    <form>
+    <input type="text" id="input1" class="input" name="input1"/>
+    <input type="text" id="input2" class="input" name="input2"/>
+    <input type="text" id="input3" class="input" name="input3"/>
+    <select name="animals" id="animals">
+    <option value="both">Both</option>
+    <option value="cats">Cats</option>
+    <option value="dogs">Dogs</option>
+    </select>
+    <button>Click</button>
+    </form>
+    `);
+    const formElement = rootElement.querySelector("form");
+    
+    const inputs = document.querySelectorAll(".input");
+    /* for (const input of inputs) {
+        input.addEventListener("input", function (event) {
+            console.log(event.target.value);
+        })
+    } */
+
+    Array.from(inputs).map(function (input) {
+        input.addEventListener("input", function (event) {
+            console.log(event.target.value);
+        })
+    })
+
+    formElement.querySelector("select").addEventListener("input", function (event) {
+        console.log(event.target.value);
+    })
+
+    formElement.addEventListener("submit", function (event) {
+        event.preventDefault();
+        console.log(event.target);
+    })
 }
 
-function initC(firstNumber, secondNumber) {
-    return firstNumber - secondNumber;
-}
-
-function initD(firstNumber, secondNumber) {
-    return firstNumber * secondNumber;
-}
-
-
-init(initD);
-
-
-
+window.addEventListener("load", loadEvent);
